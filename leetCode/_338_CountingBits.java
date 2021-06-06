63ca924eddc0f88bbef55a50f5662e7d976a5a2d
vinay <vinay@LAPTOP-BHG31BLQ>
2021-06-06 17:43:52 +0530

    Best approach using bit manipulation 338 CountingBits

--- leetCode/_338_CountingBits.java
+++ leetCode/_338_CountingBits.java
@@ -1,33 +1,48 @@
 import java.util.Arrays;
 
 public class _338_CountingBits {
-	public static int[] countBits(int n) {
-		int res[] = new int[n + 1];
-		for (int i = 0; i <= n; i++) {
-			res[i] = countOf1(i);
-		}
-
-		return res;
-	}
-
-	public static int countOf1(int n) {
-		if (n == 0) {
-			return 0;
-		}
-		if (n == 1) {
-			return 1;
-		}
-
-		int count = 1;
-		while (n != 1) {
-
-			if (n % 2 == 1) {
-				count++;
-			}
-			n = n / 2;
-		}
-		return count;
-	}
+//	public static int[] countBits(int n) {
+//		int res[] = new int[n + 1];
+//		for (int i = 0; i <= n; i++) {
+//			res[i] = countOf1(i);
+//		}
+//
+//		return res;
+//	}
+//
+//	public static int countOf1(int n) {
+//		if (n == 0) {
+//			return 0;
+//		}
+//		if (n == 1) {
+//			return 1;
+//		}
+//
+//		int count = 1;
+//		while (n != 1) {
+//
+//			if (n % 2 == 1) {
+//				count++;
+//			}
+//			n = n / 2;
+//		}
+//		return count;
+//	}
+	
+	
+	//Approach 2
+    public static int[] countBits(int n) {
+        int res[] = new int[n+1];
+        for(int i=0;i<=n;i++){
+            // note to divide by 2 -->i/2  or i>>1
+         //   res[i] = res[i/2] + (i&1);
+         //   res[i] = res[i>>1] + (i&1);
+            //note to find odd or even --> (i&1) or ((i%2==0)?0:1)
+            res[i] = res[i>>1] + ((i%2==0)?0:1);
+        }
+        
+        return res;
+    }
 	
 	
 	public static void main(String[] args) {
