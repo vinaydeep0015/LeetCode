ded3359db61bee6f519db8520127a949c7d8a0cc
vinay <vinay@LAPTOP-BHG31BLQ>
2021-06-20 15:17:33 +0530

    Comparator for sorting  map by value

---
+++ MyPracticeCode/comparator/ComparatorExample.java
@@ -0,0 +1,13 @@
+package comparator;
+import java.util.Comparator;
+import java.util.Map.Entry;
+
+public class ComparatorExample implements Comparator<Entry<Integer, Integer>> {
+
+	@Override
+	public int compare(Entry<Integer, Integer> e1, Entry<Integer, Integer> e2) {
+		return e1.getValue().compareTo(e2.getValue());
+	}
+}
+
+
